26ee068ac3318dc04a3a8987cc5474a52047e59d
Rajith Delantha <rajithgama@gmail.com>
2020-03-29 18:45:02 +0530

    initial commit

---
+++ src/main/java/com/rd/MobileRemoteJobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileRemoteJobBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileRemoteJobBackendApplication.class, args);
+	}
+
+}
